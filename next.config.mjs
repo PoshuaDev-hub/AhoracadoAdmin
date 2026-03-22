e3d0f64a7727b49872962767e51f9e0eeae81918
PoshuaDev-Hub <poshuadev@gmail.com>
2026-03-22 20:00:27 -0300

    Preparando para Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/AhorcadoMultiplayer',
-  assetPrefix: '/AhorcadoMultiplayer', 
   images: { unoptimized: true },
-  trailingSlash: true,
 };
 
 export default nextConfig;
